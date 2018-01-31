4741adce1b573fa74597aeef90daada0bcacfb4c
Yually <1213143940@qq.com>
2018-01-31 17:02:54 +0800

    commit

--- app/src/main/java/com/hanling/gitapp/MainActivity.java
+++ app/src/main/java/com/hanling/gitapp/MainActivity.java
@@ -3,17 +3,22 @@ package com.hanling.gitapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.bumptech.glide.Glide;
 
+public class MainActivity extends AppCompatActivity {
+   private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView=(ImageView)findViewById(R.id.image);
     }
 
     public void exit(View view) {
-        Toast.makeText(this, "被点击了！", Toast.LENGTH_SHORT).show();
+        String url = "http://guolin.tech/book.png";
+        Glide.with(this).load(url).into(imageView);
     }
 }
